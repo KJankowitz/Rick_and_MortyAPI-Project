b956e43a25b19b7bd464f78e8047479dcc02db6f
Kardo Jankowitz <kardojv@gmail.com>
2023-10-19 17:24:13 +0200

    post api brings back one random character fitting description - working

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const API_URL = "https://rickandmortyapi.com/api/character";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let content;
-
 app.get("/", async (req,res) => {
     var n = Math.floor(Math.random() * 826)
      const result = await axios.get(API_URL + `/${n}`);
@@ -24,34 +22,45 @@ app.get("/", async (req,res) => {
 });
 
 
-//  app.post("/", async (req, res) => {
-//      const charStatus = req.body.status;
-//      try {
-//         const answer = await axios.get(API_URL + `?status=${charStatus}`);
+app.post("/results", async (req, res) => {
+    try {
+      console.log(req.body);
+      const name = req.body.name;
+      const status = req.body.status;
+      const gender = req.body.gender;
+      
+      const response = await axios.get( API_URL + 
+        `?name=${name}&status=${status}&gender=${gender}`
+      );
+      const result = response.data.results;
+      var n = Math.floor(Math.random() * result.length)
+      console.log(result[n]);
+      res.render("index.ejs", {
+        content: result[n],
+        location: result[n].location.name
+      });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: "No characters match your criteria.",
+      });
+    }
+  });
+
+// app.post("/", async (req, res) => {
+//     const charID = req.body.id;
+//     try {
+//         const answer = await axios.get(API_URL + `/${charID}`);
 //         console.log(answer.data);
 //         res.render("index.ejs", { 
 //             content : answer.data,
-//             location : answer.data.location
-//          });
-//      } catch (error) {
+//             location : answer.data.location.name
+//         });
+//     } catch (error) {
 //         console.log(error.message);
-//      }
-//  })
-
-app.post("/", async (req, res) => {
-    const charID = req.body.id;
-    try {
-        const answer = await axios.get(API_URL + `/${charID}`);
-        console.log(answer.data);
-        res.render("index.ejs", { 
-            content : answer.data,
-            location : answer.data.location.name
-        });
-    } catch (error) {
-        console.log(error.message);
         
-    }
-})
+//     }
+// })
 
 
 app.listen(port, () => {
